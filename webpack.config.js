94fc5326320cfc0ea613badc94aba9b3a4017fff
Hyun Seung Jeong <hs9923@naver.com>
2016-12-15 09:36:16 +0900

    - webpack.config.js에 production mode uglify plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,19 @@ module.exports = {
 				exclude: /node_modules/
 			}
 		]
-	}
+	},
+	
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		})
+	]
 
 };
